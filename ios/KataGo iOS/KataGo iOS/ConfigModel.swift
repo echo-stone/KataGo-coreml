435acae6e647acdc2b00edf79cc6dd927f67c829
Chin-Chang Yang <2770271+ChinChangYang@users.noreply.github.com>
2024-07-08 19:34:50 +0800

    Refine default analysis parameters
    Adjust default analysis parameters: increase max analysis moves and interval for better balancing between analysis results and performance.

--- ios/KataGo iOS/KataGo iOS/ConfigModel.swift
+++ ios/KataGo iOS/KataGo iOS/ConfigModel.swift
@@ -56,10 +56,10 @@ extension Config {
     static let defaultBoardHeight = 19
     static let defaultKomi: Float = 7.0
     static let defaultPlayoutDoublingAdvantage: Float = 0.0
-    static let defaultAnalysisWideRootNoise: Float = 0.09375
+    static let defaultAnalysisWideRootNoise: Float = 0.03125
     static let defaultMaxMessageCharacters = 1000
-    static let defaultMaxAnalysisMoves = 36
-    static let defaultAnalysisInterval = 25
+    static let defaultMaxAnalysisMoves = 50
+    static let defaultAnalysisInterval = 50
     static let defaultMaxMessageLines = 1000
     static let defaultHiddenAnalysisVisitRatio: Float = 0.03125
 }
