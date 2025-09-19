9efcb08e307dbf1d8f545c36187df97c8e395677
tk <taeuk.works@gmail.com>
2025-09-19 14:05:00 +0900

    설정파일 업데이트

--- cpp/xcode/bench.sh
+++ cpp/xcode/bench.sh
@@ -1,1 +1,3 @@
-/Users/tk/dev/katago/KataGo-coreml/cpp/build/Release/katago benchmark -model ~/.katago/model.bin.gz -config /Users/tk/dev/katago/KataGo-coreml/cpp/configs/misc/coreml_gtp.cfg
+cd /Users/tk/dev/katago/KataGo-coreml/cpp/build
+./katago benchmark -model ./model.bin.gz -config /Users/tk/dev/katago/KataGo-coreml/cpp/configs/misc/coreml_gtp.cfg -t 5
+#/Users/tk/dev/katago/KataGo-coreml/cpp/build/Release/katago benchmark -model ~/.katago/model.bin.gz -config /Users/tk/Downloads/gtp.cfg
